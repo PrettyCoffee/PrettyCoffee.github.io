3c024362fccfbe655cb3fd51cc81d11d7a3ce87c
PrettyCoffee <jonathan-holz@outlook.com>
2020-06-19 03:05:29 +0200

    test

---
+++ precache-manifest.291805355a50fac28136cb9476a70cdd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477dde27209d910250891722c4249ffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6a506e744b6cb5c9cbc",
+    "url": "/static/css/main.c60e54e3.chunk.css"
+  },
+  {
+    "revision": "ee796f313b694f7fb533",
+    "url": "/static/js/2.e5fc0946.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5fc0946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a506e744b6cb5c9cbc",
+    "url": "/static/js/main.450e7c18.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "6752dc20c37127d44ec70b413d3a2839",
+    "url": "/static/media/Teacher.6752dc20.png"
+  },
+  {
+    "revision": "fadbe983ba3e9a7fc4840d3a09ca94b8",
+    "url": "/static/media/me-effect.fadbe983.png"
+  },
+  {
+    "revision": "40aaf72f78d1e832ec5c2b065402f912",
+    "url": "/static/media/sw-link.40aaf72f.png"
+  }
+]);
