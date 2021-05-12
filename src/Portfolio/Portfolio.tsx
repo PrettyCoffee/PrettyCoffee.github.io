ef0e3665d84f228abaaf8bad1ae82da9cae8c552
PrettyCoffee <jonathan-holz@outlook.com>
2021-05-12 23:30:45 +0200

    refactor(Portfolio): Extract social media section

--- src/Portfolio/Portfolio.tsx
+++ src/Portfolio/Portfolio.tsx
@@ -32,6 +32,7 @@ import {
 } from '../components';
 import logo from '../data/media/logo.svg';
 import { ressources } from '../data/ressources';
+import { SocialMedia } from './SocialMedia';
 
 const StalkerContainer = styled.div`
   display: flex;
@@ -132,50 +133,7 @@ export const Portfolio = () => {
 
       <ScrollspyContent icon={faHashtag} label={'follow me'}>
         <Section bgcolor={color.dark}>
-          <CardGroup>
-            <Card
-              url={'https://www.xing.com/profile/Jonathan_Holz2'}
-              icon={faXing}
-              color={color.green}
-            >
-              Jonathan Holz
-            </Card>
-            <Card
-              url={'https://github.com/PrettyCoffee'}
-              icon={faGithub}
-              color={color.dark}
-            >
-              PrettyCoffee
-            </Card>
-            <Card
-              url={'https://gitlab.com/PrettyCoffee'}
-              icon={faGitlab}
-              color={color.purple}
-            >
-              PrettyCoffee
-            </Card>
-            <Card
-              url={'https://www.youtube.com/channel/UCtQqZ6EhvprvC0H2tLN2EcA'}
-              icon={faYoutube}
-              color={color.red}
-            >
-              PrettyCoffee
-            </Card>
-            <Card
-              url={'https://www.reddit.com/user/SpinatMixxer'}
-              icon={faReddit}
-              color={color.orange}
-            >
-              u/SpinatMixxer
-            </Card>
-            <Card
-              url={'https://steamcommunity.com/id/PrettyCoffee/'}
-              icon={faSteam}
-              color={color.blue}
-            >
-              PrettyCoffee
-            </Card>
-          </CardGroup>
+          <SocialMedia />
         </Section>
       </ScrollspyContent>
 
---
+++ src/Portfolio/SocialMedia.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import { useTheme } from '@emotion/react';
+import {
+  faYoutube,
+  faGithub,
+  faGitlab,
+  faReddit,
+  faXing,
+  faSteam,
+} from '@fortawesome/free-brands-svg-icons';
+
+import { Card, CardGroup, Section } from '../components';
+
+export const SocialMedia = () => {
+  const { color } = useTheme();
+  return (
+    <Section bgcolor={color.dark}>
+      <CardGroup>
+        <Card
+          url={'https://www.xing.com/profile/Jonathan_Holz2'}
+          icon={faXing}
+          color={color.green}
+        >
+          Jonathan Holz
+        </Card>
+        <Card
+          url={'https://github.com/PrettyCoffee'}
+          icon={faGithub}
+          color={color.dark}
+        >
+          PrettyCoffee
+        </Card>
+        <Card
+          url={'https://gitlab.com/PrettyCoffee'}
+          icon={faGitlab}
+          color={color.purple}
+        >
+          PrettyCoffee
+        </Card>
+        <Card
+          url={'https://www.youtube.com/channel/UCtQqZ6EhvprvC0H2tLN2EcA'}
+          icon={faYoutube}
+          color={color.red}
+        >
+          PrettyCoffee
+        </Card>
+        <Card
+          url={'https://www.reddit.com/user/SpinatMixxer'}
+          icon={faReddit}
+          color={color.orange}
+        >
+          u/SpinatMixxer
+        </Card>
+        <Card
+          url={'https://steamcommunity.com/id/PrettyCoffee/'}
+          icon={faSteam}
+          color={color.blue}
+        >
+          PrettyCoffee
+        </Card>
+      </CardGroup>
+    </Section>
+  );
+};
